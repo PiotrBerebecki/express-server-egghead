04b71c5b1639602757ab33f02f7506c34b5af96c
PiotrBerebecki <petebere@gmail.com>
2017-07-08 01:23:09 +0100

    Serve images and css

--- README.md
+++ README.md
@@ -5,7 +5,31 @@ Code along with [Getting Started with Express.js course on egghead.io](https://e
 ## Notes
 
 - `npm init -y` to accept defaults
+
 - `npm i -D nodemon` shorthand for `npm install --dev nodemon`
+
 - Express uses [path-to-regexp](https://www.npmjs.com/package/path-to-regexp) for matching the route paths; see the path-to-regexp documentation for all the possibilities in defining route paths.
+
+```js
+// starting with mary
+app.get(/^\/mary/, (req, res, next) => {
+  console.log('starting with mary');
+  next();
+});
+
+// wal anywhere in route
+app.get(/wal/, (req, res, next) => {
+  console.log('wal anywhere in route');
+  next();
+```
+});
+
 - To observe changes in hbs files using nodemon:
 `"nodemon server.js -e js,hbs"`
+
+- To serve static files such as images, CSS files, and JavaScript files, use the express.static built-in middleware function in Express.
+
+Pass the name of the directory that contains the static assets to the express.static middleware function to start serving the files directly. For example, use the following code to serve images, CSS files, and JavaScript files in a directory named public:
+```js
+app.use(express.static('public'));
+```
--- index.js
+++ index.js
@@ -12,42 +12,27 @@ fs.readFile(path.join(__dirname, 'users.json'), 'utf8', (err, data) => {
 });
 
 const app = express();
+
+app.use(express.static('public'));
+
 app.set('view engine', 'hbs');
 hbs.registerPartials(path.join(__dirname, 'views', 'partials'));
 hbs.registerHelper('uppercase', uppercase);
 hbs.registerHelper('propercase', propercase);
 
 app.get('/', (req, res) => {
-  // const html = users
-  //   .map(user => {
-  //     return `<a href="/${user.email}">${user.name.full}</a></br>`;
-  //   })
-  //   .join('');
-
   res.render('home', {
     pageTitle: 'Handlebars',
     siteHeading: 'Welcome to Handlebars',
     pageHeading: 'List of users',
-    users: users,
     siteFooter: 'Contact us @UsersPage',
+    users,
   });
 });
 
-// starting with mary
-app.get(/^\/mary/, (req, res, next) => {
-  console.log('starting with mary');
-  next();
-});
-
-// wal anywhere in route
-app.get(/wal/, (req, res, next) => {
-  console.log('wal anywhere in route');
-  next();
-});
-
-app.get('/:email', (req, res) => {
-  const { email } = req.params;
-  res.send(email);
+app.get('/:username', (req, res) => {
+  const { username } = req.params;
+  res.render('user', { username });
 });
 
 const PORT = process.env.PORT || 3000;
